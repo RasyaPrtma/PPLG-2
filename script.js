931212afc64814ba96b900f5933b546eef5c1e0e
Kubrooo <galihtriardiansyah@gmail.com>
2024-06-12 10:08:41 +0700

    Update Responsive

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
+    menu.classList.toggle('open');
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
 
 window.onscroll = () => {
+    menu.classList.remove('open');
     menu.classList.remove('bx-x');
     navbar.classList.remove('open');
 };
